dba97bb76835b0700e9e09256aaecdd02ca7ab1c
loudagnese <louis.dagnese@gmail.com>
2022-10-02 12:44:48 -0700

    Engineer class finished

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,3 +5,15 @@ test(`Can set Github username via constructor`, () => {
   const eng = new Engineer(1, `lou`, `email`, userName);
   expect(eng.githubName).toBe(userName);
 });
+
+test(`Can get Github username via getGitHub()`, () => {
+    const userName = `Foo`;
+    const eng = new Engineer(1, `lou`, `email`, userName);
+    expect(eng.getGitHub()).toBe(userName);
+})
+
+test(`getRole() should return "Engineer"`, () => {
+    const roleTest = "Engineer";
+    const eng = new Engineer(1, `lou`, `email`, `name`);
+    expect(eng.getRole()).toBe(roleTest);
+  });
