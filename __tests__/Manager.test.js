05d0be4837ab252dbf05f2e235bfd05b387e401b
loudagnese <louis.dagnese@gmail.com>
2022-10-02 12:31:29 -0700

    finish Manager Class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,7 +7,13 @@ test("Can set the officeNumber of a Manager", () => {
 });
 
 test(`getRole() should return "Manager"`, () => {
-    const roleTest = "Manager";
-    const manager = new Manager(1,`lou`, `email`, 1);
-    expect(manager.getRole()).toBe(roleTest);
-})
+  const roleTest = "Manager";
+  const manager = new Manager(1, `lou`, `email`, 1);
+  expect(manager.getRole()).toBe(roleTest);
+});
+
+test(`Can get officeNumber using getOfficeNumber()`, () => {
+  const officeTest = 1;
+  const manager = new Manager(1, `lou`, `email`, officeTest);
+  expect(manager.getOfficeNumber()).toBe(officeTest);
+});
