cf30b830daf7bace090d95b5c4062a7138bcbfa3
loudagnese <louis.dagnese@gmail.com>
2022-10-02 11:50:27 -0700

    set name, id and email with test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,3 +4,13 @@ test("Instantiate Employee object", () => {
   const emp = new Employee();
   expect(typeof emp).toBe("object");
 });
+
+test("Set Employee name, id and email", () => {
+    const name = "Louis";
+    const id = 1;
+    const email = "louis@email.com";
+    const emp = new Employee(id,name, email);
+    expect(emp.name).toBe(name);
+    expect(emp.id).toBe(id);
+    expect(emp.email).toBe(email);
+})
