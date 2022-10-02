e70fdf5615332d2799961fe1dc9617e0a9286b9c
loudagnese <louis.dagnese@gmail.com>
2022-10-02 11:54:59 -0700

    set up test and method for getName()

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,11 +6,17 @@ test("Instantiate Employee object", () => {
 });
 
 test("Set Employee name, id and email", () => {
-    const name = "Louis";
-    const id = 1;
-    const email = "louis@email.com";
-    const emp = new Employee(id,name, email);
-    expect(emp.name).toBe(name);
-    expect(emp.id).toBe(id);
-    expect(emp.email).toBe(email);
-})
+  const name = "Louis";
+  const id = 1;
+  const email = "louis@email.com";
+  const emp = new Employee(id, name, email);
+  expect(emp.name).toBe(name);
+  expect(emp.id).toBe(id);
+  expect(emp.email).toBe(email);
+});
+
+test("Can get name via getName()", () => {
+  const name = "louis";
+  const emp = new Employee(1, name, "email");
+  expect(emp.getName()).toBe(name);
+});
